83374bb306b4f9e0475bf0715879ef814716e496
James Tingen <jotingen@gmail.com>
2021-05-09 23:26:34 -0400

    Start adding in races/attributes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,126 @@
 use rand::Rng;
 use regex::Regex;
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+struct Abilities {
+    strength: u32,
+    dexterity: u32,
+    constitution: u32,
+    intellect: u32,
+    wisdom: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+struct RaceAbilities {
+    description: String,
+    abilities: Abilities,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+struct RaceAge {
+    description: String,
+    adulthood: u32,
+    lifespan: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+enum Alignment {
+    LG, //Lawful good
+    NG, //Neutral good
+    CG, //Chaotic good
+    LN, //Lawful neutral
+    N,  //Neutral
+    CN, //Chaotic neutral
+    LE, //Lawful evil
+    NE, //Neutral evil
+    CE, //Chaotic evil
+}
+impl Default for Alignment {
+    fn default() -> Self {
+        Alignment::N
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+struct RaceAlignment {
+    description: String,
+    alignent: Alignment,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+enum SizeClass {
+    Small,
+    Medium,
+    Large,
+}
+impl Default for SizeClass {
+    fn default() -> Self {
+        SizeClass::Medium
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+struct RaceSize {
+    description: String,
+    lower: f32,
+    upper: f32,
+    class: SizeClass,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+struct RaceNames {
+    description: String,
+    child: Vec<String>,
+    male: Vec<String>,
+    female: Vec<String>,
+    clan: Vec<String>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+struct RaceSpeed {
+    description: String,
+    speed: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+struct RaceModifier {
+    description: String,
+    modifier: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+struct SubRace {
+    sub_race: String,
+    description: String,
+    ability_score_increase: Abilities,
+    modifiers: Vec<RaceModifier>,
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+struct Race {
+    race: String,
+    description: String,
+    names: RaceNames,
+    ability_score_increase: RaceAbilities,
+    age: RaceAge,
+    alignment: RaceAlignment,
+    size: RaceSize,
+    speed: RaceSpeed,
+    modifiers: Vec<RaceModifier>,
+    languages: Vec<String>,
+    subraces: Vec<SubRace>,
+}
+
+#[derive(Serialize, Deserialize, Debug, Default)]
+struct Character {
+    name: String,
+    race: Race,
+    age: u32,
+    alignment: Alignment,
+    ability_score_base: Abilities,
+}
 
 fn main() {
     println!("Hello, world!");
@@ -15,6 +136,11 @@ fn main() {
         roll_stats(&mut rolls);
     }
     println!("Using stats {:?}", rolls);
+
+    //Load races
+    let race_json = include_str!("races.json");
+    let races: Vec<Race> = serde_json::from_str(&race_json).unwrap();
+    println!("{:#?}", races)
 }
 
 fn roll_stats(rolls: &mut [u8; 6]) {
